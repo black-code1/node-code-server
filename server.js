32178fb68b910432127559ebc96e54d74961162e
black-code1 <legrandmahamat1@gmail.com>
2021-07-15 05:03:15 +0100

    send html pages to the browser

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 // require http module - core module
 const http = require('http');
 
+// require file system module
+const fs = require('fs');
+
 // create a server with the http module
 // the callback function runs everytime the request comes in to our server
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('content-type', 'text/html');
+
+  // send an html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if(err){
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  })
 });
 
 // invoke the listen method
