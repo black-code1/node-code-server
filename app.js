d407703f8180d284104d71ef2ed88a1b47df54e1
black-code1 <legrandmahamat1@gmail.com>
2021-07-15 06:46:21 +0100

    redirect and 404 page

--- app.js
+++ app.js
@@ -16,3 +16,13 @@ app.get('/about', (req, res) => {
   //res.send('<p>about page</p>');
   res.sendFile('./views/about.html', { root: __dirname});
 });
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 page - use to create middleware & fire middleware functions in express
+app.use('', (req, res) =>{
+  res.status(404).sendFile('./views/404.html', { root: __dirname})
+})
