63a518ee1c495b49f591a4f87724cd2c88de6bec
black-code1 <legrandmahamat1@gmail.com>
2021-07-15 07:18:17 +0100

    creating different views

--- app.js
+++ app.js
@@ -12,21 +12,18 @@ app.set('view engine', 'ejs')
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  //res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname});
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  //res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname});
+  res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
+})
 
 // 404 page - use to create middleware & fire middleware functions in express
-app.use('', (req, res) =>{
-  res.status(404).sendFile('./views/404.html', { root: __dirname})
-})
+app.use((req, res) =>{
+  res.status(404).render('404');
+});
