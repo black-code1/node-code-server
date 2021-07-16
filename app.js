8f4f56387571fd10a858db285f869f320443c9f1
black-code1 <legrandmahamat1@gmail.com>
2021-07-16 10:52:27 +0100

    saving and getting data from mongodb

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose'); 
+const Blog = require('./models/blog');
 // setup express app - invoke express to create an instance of express app
 const app = express();
 
@@ -24,6 +25,45 @@ app.use(express.static('public'))
 
 // morgan middleware
 app.use(morgan('dev'))
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+
+  blog.save()
+      .then((result) => {
+        res.send(result)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+});
+
+app.get('/all-blogs', (req, res) => {
+  Blog
+    .find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/single-blog', (req, res) => {
+  Blog
+    .findById('60f15368635d5e48d11978bb')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 app.get('/', (req, res) => {
   const blogs = [
