ed872eda54e88b11c88cb8a4aff1f11aef85b9b1
black-code1 <legrandmahamat1@gmail.com>
2021-07-16 15:36:20 +0100

    Done with MVC

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -6,7 +6,7 @@ const blog_index = (req, res) => {
     .find()
     .sort({ createdAt: -1})
     .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result })
+      res.render('blogs/index', { title: 'All Blogs', blogs: result })
     })
     .catch((err) => {
       console.log(err)
@@ -18,7 +18,7 @@ const blog_details = (req, res) => {
   Blog
     .findById(id)
     .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' })
+      res.render('blogs/details', { blog: result, title: 'Blog Details' })
     })
     .catch(err => {
       console.log(err);
@@ -26,7 +26,7 @@ const blog_details = (req, res) => {
 }
 
 const blog_create_get = (req, res) => {
-  res.render('create', {title: 'Create a new Blog'});
+  res.render('blogs/create', {title: 'Create a new Blog'});
 }
 
 const blog_create_post = (req, res) => {
