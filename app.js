ab0011c5ad0d9c2c9d36c48fd358af803c3d8e16
black-code1 <legrandmahamat1@gmail.com>
2021-07-16 03:18:43 +0100

    Introduced next()

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ app.set('view engine', 'ejs')
 app.listen(3000);
 
 // middleware
-app.use((req, res) => {
-  console.log('new request mad:');
+app.use((req, res, next) => {
+  console.log('new request made:');
   console.log('host: ', req.hostname);
   console.log('path: ', req.path);
   console.log('method: ', req.method);
+  next();
+})
+
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+ 
+  next();
 })
 
 app.get('/', (req, res) => {
